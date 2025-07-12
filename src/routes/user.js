1f100ca038fbfeb1a8b9019e07c0bfebc643702c
Manoranjan-parida247 <manoranjanparida247@gmail.com>
2025-07-12 17:16:14 +0530

    complete "/user/requests/received" api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const { userAuth } = require('../middlewares/auth');
-
+const ConnectionRequest = require('../models/connectionRequest')
 
 const userRouter = express.Router();
 
 
 //user rrquest api-> get all the pending connection request for loggedIn use
-userRouter.get("/user/request", userAuth, async(req, res) => {
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
+
+
+        const connectionRequests = await ConnectionRequest.find({
+            toUserId: loggedInUser._id,
+            status: "interested"
+        }).populate("fromUserId", ["firstName", "lastName"]);
+        // populate("fromUserId", "firstName lastName");
+
+        res.status(200).json({
+            message: "Data fetched successfully!",
+            data: connectionRequests
+        })
     } catch (error) {
-        
+        console.error("Error sending connection request:", err);
+        return res.status(500).json({
+            statusCode: 500,
+            message: "Internal server error",
+            error: err.message
+        })
     }
 })
 
